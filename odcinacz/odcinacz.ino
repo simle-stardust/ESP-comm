8445d29f42bc019f263754e750230679a6a68688
yasiupl <contact@yasiu.pl>
2019-03-13 13:32:27 +0100

    podstawowa komunikacja UART-UART over TCP

--- odcinacz/odcinacz.ino
+++ odcinacz/odcinacz.ino
@@ -1,13 +1,29 @@
-#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+char startMarker = '@';
+char endMarker = '!';
+
+
+#define bufferSize 8192
+const byte bufferCharSize = 255;
+char incomingBuffer[bufferCharSize];
+char outgoingBuffer[bufferSize];
+static boolean recvInProgress = false;
+static byte ndx = 0;
+char rc;
 
 const char *ssid = "Hermes";
 const char *password = "getmesomemilk";
 
-IPAddress ip(192, 168, 4, 3);
+
 IPAddress gateway(192, 168, 4, 1);
+IPAddress ip(192, 168, 4, 2);
+IPAddress antares(192, 168, 4, 3);
 IPAddress subnet(255, 255, 255, 0);
 
+ESP8266WebServer server(80);
 HTTPClient http;
 
 void setup()
@@ -17,6 +33,9 @@ void setup()
   Serial.println("Welcome to Hernes.");
 
   connectToAP();
+
+  server.on("/data", handleData);
+  server.begin();
 }
 
 void loop()
@@ -27,7 +46,7 @@ void loop()
     // Reconnect
     Serial.println("Disconnected.");
     connectToAP();
-    while(1)
+    while (1)
     {
       if (WiFi.status() == WL_CONNECTED)
         break;
@@ -35,16 +54,43 @@ void loop()
   }
   else
   {
-    http.begin("http://" + Ip2Str(gateway) + ":80/data");
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    int httpCode = http.POST("data=Hello From ESP8266");  
-   String payload = http.getString();                  
- 
-   Serial.println(httpCode);   //Print HTTP return code
-   Serial.println(payload);    //Print request response payload
- 
-   http.end();
-   delay(1000);
+
+    server.handleClient();
+
+    if (Serial.available() > 0)
+    {
+      rc = Serial.read();
+
+      if (recvInProgress == true)
+      {
+        if (rc != endMarker)
+        {
+          incomingBuffer[ndx] = rc;
+          ndx++;
+          if (ndx >= bufferCharSize)
+          {
+            ndx = bufferCharSize - 1;
+          }
+        }
+        else
+        {
+          incomingBuffer[ndx] = '\0'; // terminate the string
+          recvInProgress = false;
+
+          Serial.print("Received From Serial: ");
+          Serial.println(incomingBuffer);
+
+          sendDataTo(gateway, incomingBuffer);
+          
+          ndx = 0;
+        }
+      }
+
+      else if (rc == startMarker)
+      {
+        recvInProgress = true;
+      }
+    }
   }
 }
 
@@ -52,7 +98,7 @@ void loop()
 bool connectToAP()
 {
   Serial.println("Connecting ...");
-  WiFi.disconnect();
+  //WiFi.disconnect();
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password); //Connect to access point
   WiFi.config(ip, gateway, subnet);
@@ -69,11 +115,26 @@ bool connectToAP()
   Serial.println(WiFi.localIP());
 }
 
+void handleData() {
+  server.send(200, "text/html", "OK");
+  Serial.print("Received From " + server.client().remoteIP().toString() + ": ");
+  Serial.println(server.arg("payload"));
+}
+
+void sendDataTo(const IPAddress& ip, String message) {
+  http.begin("http://" + Ip2Str(ip) + ":80/data");
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpCode = http.POST("payload=" + message);
+  String payload = http.getString();
+  String response = String(httpCode) + " " + String(payload);
+  http.end();
+  Serial.println(response);
+}
 
 String Ip2Str(const IPAddress& ipAddress)
 {
-  return String(ipAddress[0]) + String(".") +\
-  String(ipAddress[1]) + String(".") +\
-  String(ipAddress[2]) + String(".") +\
-  String(ipAddress[3])  ; 
+  return String(ipAddress[0]) + String(".") + \
+         String(ipAddress[1]) + String(".") + \
+         String(ipAddress[2]) + String(".") + \
+         String(ipAddress[3]);
 }
--- server/server.ino
+++ server/server.ino
@@ -1,37 +1,142 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+char startMarker = '@';
+char endMarker = '!';
+
+
+#define bufferSize 8192
+const byte bufferCharSize = 255;
+char incomingBuffer[bufferCharSize];
+char outgoingBuffer[bufferSize];
+static boolean recvInProgress = false;
+static byte ndx = 0;
+char rc;
 
 const char *ssid = "Hermes";
 const char *password = "getmesomemilk";
 
-ESP8266WebServer server(80);
 
-/* Test Message, Please Ignore*/
-void handleRoot() {
-	server.send(200, "text/html", "<h1>Hermes reporting for duty</h1>");
-}
+IPAddress ip(192, 168, 4, 1);
+IPAddress odcinacz(192, 168, 4, 2);
+IPAddress antares(192, 168, 4, 3);
+IPAddress subnet(255, 255, 255, 0);
+
+/* Pamięć: */
+// Gondola Główna
+typedef struct {
+  int8_t hour = 0x00;
+  int8_t minute = 0x00;
+  int8_t second = 0x00;
+  int16_t DS18B20[4] = {0x0000};
+  int16_t humidity = 0x0000;
+  int16_t pressure = 0x0000;
+  int32_t lattitude = 0x00000000;
+  int32_t longtitude = 0x00000000;
+  int16_t RTD[30] = {0x0000};
+  int16_t mosfet = 0x0000;
+  int16_t flag_main = 0x0000;
+  int16_t flag_antares = 0x0000;
+} frame_main;
+
+/*
+  // Gondola Experymentalna
+  typedef struct {
+  int16_t RTD[30] = {0x0000};
+  int16_t mosfet = 0x0000;
+  int16_t flag = 0x0000;
+
+  } frame_antares;
+*/
+
+//frame_main Memory;
+
+ESP8266WebServer server(80);
+HTTPClient http;
 
 void setup() {
-	Serial.begin(115200);
-	Serial.println();
-	Serial.print("Hermes reporting for duty");
-  
-	WiFi.softAP(ssid, password);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Hermes reporting for duty");
 
-	IPAddress myIP = WiFi.softAPIP();
-	Serial.print("AP IP address: ");
-	Serial.println(myIP);
-	server.on("/", handleRoot);
-	server.on("/data", handleData);
-	server.begin();
-	Serial.println("HTTP server started");
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+
+  server.on("/", handleRoot);
+  server.on("/data", handleData);
+  server.begin();
+  Serial.println("HTTP server started");
+
 }
 
 void loop() {
-	server.handleClient();
+  server.handleClient();
+
+  if (Serial.available() > 0)
+  {
+    rc = Serial.read();
+
+    if (recvInProgress == true)
+    {
+      if (rc != endMarker)
+      {
+        incomingBuffer[ndx] = rc;
+        ndx++;
+        if (ndx >= bufferCharSize)
+        {
+          ndx = bufferCharSize - 1;
+        }
+      }
+      else
+      {
+        incomingBuffer[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+
+        Serial.print("Received From Serial: ");
+        Serial.println(incomingBuffer);
+
+        sendDataTo(odcinacz, incomingBuffer);
+
+        ndx = 0;
+      }
+    }
+
+    else if (rc == startMarker)
+    {
+      recvInProgress = true;
+    }
+  }
 }
 
+void handleRoot() {
+  server.send(200, "text/html", "<h1>Hermes reporting for duty</h1>");
+}
 
 void handleData() {
-	server.send(200, "text/plain", "Bounced: " + server.arg("data"));
+  server.send(200, "text/html", "OK");
+  Serial.print("Received From " + server.client().remoteIP().toString() + ": ");
+  Serial.println(server.arg("payload"));
+}
+
+void sendDataTo(const IPAddress& ip, String message) {
+  http.begin("http://" + Ip2Str(ip) + ":80/data");
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpCode = http.POST("payload=" + message);
+  String payload = http.getString();
+  String response = String(httpCode) + " " + String(payload);
+  http.end();
+  Serial.println(response);
+}
+
+String Ip2Str(const IPAddress& ipAddress)
+{
+  return String(ipAddress[0]) + String(".") + \
+         String(ipAddress[1]) + String(".") + \
+         String(ipAddress[2]) + String(".") + \
+         String(ipAddress[3]);
 }
