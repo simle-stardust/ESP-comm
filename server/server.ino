4a50ab3e813cd056886467cb99bc0eda1a11a277
yasiupl <contact@yasiu.pl>
2019-05-18 10:29:48 +0200

    main gondola uart communications

--- server/server.ino
+++ server/server.ino
@@ -160,7 +160,7 @@ void processMessage(String Message) {
   if(Message.indexOf(command) >= 0) {
       Message.remove(0, Message.indexOf(command) + command.length());
       Message.toCharArray(temp, data.length()+1);
-      sscanf(temp, "%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd,%hd", &Memory.RTD[0],&Memory.RTD[1],&Memory.RTD[2],&Memory.RTD[3],&Memory.RTD[4],&Memory.RTD[5],&Memory.RTD[6],&Memory.RTD[7],&Memory.RTD[8],&Memory.RTD[9],&Memory.RTD[10],&Memory.RTD[11],&Memory.RTD[12],&Memory.RTD[13],&Memory.RTD[14],&Memory.RTD[15],&Memory.RTD[16],&Memory.RTD[17],&Memory.RTD[18],&Memory.RTD[19],&Memory.RTD[20],&Memory.RTD[21],&Memory.RTD[22],&Memory.RTD[23],&Memory.RTD[24],&Memory.RTD[25],&Memory.RTD[26],&Memory.RTD[27],&Memory.RTD[28],&Memory.RTD[29], &Memory.mosfet[0],&Memory.mosfet[1],&Memory.mosfet[2],&Memory.mosfet[3],&Memory.mosfet[4],&Memory.mosfet[5],&Memory.mosfet[6],&Memory.mosfet[7],&Memory.mosfet[8],&Memory.mosfet[9],&Memory.mosfet[10],&Memory.mosfet[11], &Memory.flag_antares);
+      sscanf(temp, "%c,%c,%c,%hd,%hd,%hd,%hd,%hd,%hd,%d,%d,%d,%hd", &Memory.hour,&Memory.minute,&Memory.second,&Memory.DS18B20[1],&Memory.DS18B20[2],&Memory.DS18B20[3],&Memory.DS18B20[4],&Memory.humidity,&Memory.pressure,&Memory.lattitude,&Memory.longtitude,&Memory.altitude,&Memory.flag_main);
    }
 }
 
